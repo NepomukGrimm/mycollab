ddfd9fc4d255a83ee9fcd86ac6ba4b7f5e25993e
NepomukGrimm <nepomukgrimm@gmx.de>
2017-04-04 19:44:14 +0200

    composer update, add test file

---
+++ test/ExampleTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JahnHane
+ * Date: 04.04.2017
+ * Time: 19:42
+ */
+
+namespace AppTest;
+
+
+class ExampleTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * Assert that true is true
+     * @test
+     */
+    public function testSystemRuns() {
+        $this->assertTrue(true);
+    }
+
+}
